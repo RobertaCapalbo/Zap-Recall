dbe8e412b32df9000108aae368088e056eaafade
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-03-01 20:43:30 -0300

    ajustes finos de styled components

--- src/components/App.js
+++ src/components/App.js
@@ -9,134 +9,354 @@ import React, { useState } from 'react';
 
 export default function App() {
     const [number, setNumber] = useState(0)
+    const [questionOne, setQuestionOne] = useState(false);
+    const [questionTwo, setQuestionTwo] = useState(false);
+    const [questionThree, setQuestionThree] = useState(false);
+    const [questionFour, setQuestionFour] = useState(false);
+    const [answerOne, setAnswerOne] = useState(false);
+    const [answerTwo, setAnswerTwo] = useState(false);
+    const [answerThree, setAnswerThree] = useState(false);
+    const [answerFour, setAnswerFour] = useState(false);
+
     return(
 <div ClassName="tela">
-<div className="container">
+<Container>
 
-        <div className="topo">
-            <img className="logo" src={logo} alt='logo'/>
-            <div className='nome'>ZapRecall</div>
-        </div>
+        <Topo>
+            <Logo src={logo} alt='logo'/>
+            <Nome>ZapRecall</Nome>
+        </Topo>
 
 <div className='perguntas'>
 
      <div className="pergunta-um">
-        <div className='pergunta'>
-            <div className="front-face">
-                <p className='pergunta-number'>Pergunta 1</p>
-                <img className="seta-play" src={seta_play} alt='seta-play'/>
-            </div>
-        </div>
-        <div className="back-face none">
-            <p className='pergunta-question'>Quando é o aniversário de Harry Potter?</p>
-            <img className="seta-virar" src={seta_virar} alt='seta-virar'/>
-        </div>
-        <div className="answers none">
-            <p className='resposta-question'>31 de julho</p>
-            <div className="buttons">
-            <button className="no">
+        <Pergunta>
+            <Frontface>
+                <Question>Pergunta 1</Question>
+                <SetaPlay src={seta_play} alt='seta-play'/>
+            </Frontface>
+        </Pergunta>
+        <Backface>
+            <ZapQuestion>Quando é o aniversário de Harry Potter?</ZapQuestion>
+            <SetaVirar src={seta_virar} alt='seta-virar'/>
+        </Backface>
+        <Answers>
+            <ZapAnswer>31 de julho</ZapAnswer>
+            <Buttons>
+            <No>
                 Não lembrei
-            </button>
-            <button className="almost">
+            </No>
+            <Almost>
                 Quase não lembrei
-            </button>
-            <button className="yes">
+            </Almost>
+            <Yes>
                 Zap!
-            </button>
-          </div>
-        </div>
+            </Yes>
+          </Buttons>
+        </Answers>
     </div>
 
 
     <div className="pergunta-dois">
-        <div className='pergunta'>
-            <div className="front-face">
-                <p className='pergunta-number'>Pergunta 2</p>
-                <img className="seta-play" src={seta_play} alt='seta-play'/>
-            </div>
-        </div>
-        <div className="back-face none">
-            <p className='pergunta-question'>Qual era o animago de Lupin?</p>
-            <img className="seta-virar" src={seta_virar} alt='seta-virar'/>
-        </div>
-        <div className="answers none">
-            <p className='resposta-question'>Pegadinha! Lupin não era um animago, mas um lobisomem</p>
-            <div className="buttons">
-            <button className="no">
+        <Pergunta>
+            <Frontface>
+                <Question>Pergunta 2</Question>
+                <SetaPlay src={seta_play} alt='seta-play'/>
+            </Frontface>
+        </Pergunta>
+        <Backface>
+            <ZapAnswer>Qual era o animago de Lupin?</ZapAnswer>
+            <SetaVirar src={seta_virar} alt='seta-virar'/>
+        </Backface>
+        <Answers>
+            <ZapQuestion>Pegadinha! Lupin não era um animago, mas um lobisomem</ZapQuestion>
+            <Buttons>
+            <No>
                 Não lembrei
-            </button>
-            <button className="almost">
+            </No>
+            <Almost>
                 Quase não lembrei
-            </button>
-            <button className="yes">
+            </Almost>
+            <Yes>
                 Zap!
-            </button>
-          </div>
-        </div>
+            </Yes>
+          </Buttons>
+        </Answers>
     </div>
 
     <div className="pergunta-tres">
-        <div className='pergunta'>
-            <div className="front-face">
-                <p className='pergunta-number'>Pergunta 3</p>
-                <img className="seta-play" src={seta_play} alt='seta-play'/>
-            </div>  
-        </div>
-        <div className="back-face none">
-            <p className='pergunta-question'>Qual foi a segunda horcrux a ser destruída?</p>
-            <img className="seta-virar" src={seta_virar} alt='seta-virar'/>
-        </div>
-        <div className="answers none">
-            <p className='resposta-question'>O anel</p>
-            <div className="buttons">
-            <button className="no">
+        <Pergunta>
+            <Frontface>
+                <Question>Pergunta 3</Question>
+                <SetaPlay src={seta_play} alt='seta-play'/>
+            </Frontface>  
+        </Pergunta>
+        <Backface>
+            <ZapQuestion>Qual foi a segunda horcrux a ser destruída?</ZapQuestion>
+            <SetaVirar src={seta_virar} alt='seta-virar'/>
+        </Backface>
+        <Answers>
+            <ZapAnswer>O anel</ZapAnswer>
+            <Buttons>
+            <No>
                 Não lembrei
-            </button>
-            <button className="almost">
+            </No>
+            <Almost>
                 Quase não lembrei
-            </button>
-            <button className="yes">
+            </Almost>
+            <Yes>
                 Zap!
-            </button>
-          </div>
-        </div>
+            </Yes>
+          </Buttons>
+        </Answers>
     </div>
 
 
     <div className="pergunta-quatro">
-        <div className='pergunta'>
-            <div className="front-face">
-                <p className='pergunta-number'>Pergunta 4</p>
-                <img className="seta-play" src={seta_play} alt='seta-play'/>
-            </div>  
-        </div>
-        <div className="back-face none">
-            <p className='pergunta-question'>Qual era a posição de Tiago Potter no time de quadribol da Grifinória?</p>
-            <img className="seta-virar" src={seta_virar} alt='seta-virar'/>
-        </div>
-        <div className="answers none">
-            <p className='resposta-question'>Ele era artilheiro! Já seu filho, apanhador.</p>
-            <div className="buttons">
-            <button className="no">
+        <Pergunta>
+            <Frontface>
+                <Question>Pergunta 4</Question>
+                <SetaPlay src={seta_play} alt='seta-play'/>
+            </Frontface>  
+        </Pergunta>
+        <Backface>
+            <ZapQuestion>Qual era a posição de Tiago Potter no time de quadribol da Grifinória?</ZapQuestion>
+            <SetaVirar src={seta_virar} alt='seta-virar'/>
+        </Backface>
+        <Answers>
+            <ZapAnswer>Ele era artilheiro! Já seu filho, apanhador.</ZapAnswer>
+            <Buttons>
+            <No>
                 Não lembrei
-            </button>
-            <button className="almost">
+            </No>
+            <Almost>
                 Quase não lembrei
-            </button>
-            <button className="yes">
+            </Almost>
+            <Yes>
                 Zap!
-            </button>
-          </div>
-        </div>
+            </Yes>
+          </Buttons>
+        </Answers>
     </div>
 
 </div>
-</div>
+</Container>
 
-    <div className='footer'>
-        <p className='concluidos'>{number}/4 CONCLUÍDOS</p>
-    </div>
+    <Footer
+    >
+        <Concluidos>{number}/4 CONCLUÍDOS</Concluidos>
+    </Footer>
 
  </div>
  )
 }
+
+const Container = styled.div`
+width:100vw;
+height:682px;
+background-color:#FB6B6B;
+display:flex;
+flex-direction: column;
+align-items: center;
+position:relative;
+overflow-y: scroll;
+&&::-webkit-scrollbar{
+    display:none;
+}
+`;
+
+const Topo = styled.div`
+display:flex;
+align-items: center;
+gap:20px;
+margin-top:48px;
+margin-bottom:59px;
+`;
+
+const Nome = styled.div`
+width: 203.17px;
+height: 44px;
+font-family: 'Righteous';
+font-style: normal;
+font-weight: 400;
+font-size: 36px;
+line-height: 45px;
+display: flex;
+align-items: center;
+text-align: center;
+letter-spacing: -0.012em;
+color: #FFFFFF;
+`;
+
+const Logo = styled.img`
+width: 52px;
+height: 60px;
+`;
+
+const Pergunta = styled.div`
+width: 300px;
+height: 65px;
+background: #FFFFFF;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+display:flex;
+align-items: center;
+justify-content: center;
+gap:163px;
+margin-bottom:25px;
+`;
+
+const Frontface = styled.div`
+display:flex;
+align-items: center;
+justify-content: center;
+gap:163px;
+`;
+
+const Question = styled.p`
+width: 87px;
+height: 19px;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 19px;
+color: #333333;
+`;
+
+const SetaPlay = styled.img`
+width: 20px;
+height: 23px;
+`;
+
+const Footer = styled.div`
+width: 100%;
+height: 70px;
+background: #FFFFFF;
+box-shadow: 0px -4px 6px rgba(0, 0, 0, 0.05);
+bottom:0;
+left:0;
+position:absolute;
+text-align:center;
+display:flex;
+align-items: center;
+justify-content: center;
+position:fixed;
+`;
+
+const Concluidos = styled.p`
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 22px;
+color: #333333;
+`;
+
+const Backface = styled.div`
+background: #FFFFD5;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+width: 300px;
+height: 131px;
+margin-bottom:23px;
+position:relative;
+display:none
+`;
+
+const Answers = styled.div`
+background: #FFFFD5;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+width: 300px;
+height: 131px;
+margin-bottom:23px;
+position:relative;
+display:none
+`;
+
+const SetaVirar = styled.img`
+position:absolute;
+bottom:10px;
+left:255px;
+width: 30px;
+height: 20px;
+`;
+
+const ZapQuestion = styled.p`
+background: #FFFFD5;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+width: 300px;
+height: 131px;
+margin-bottom:23px;
+position:relative;
+display:none
+`;
+
+const ZapAnswer = styled.p`
+background: #FFFFD5;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+width: 300px;
+height: 131px;
+margin-bottom:23px;
+position:relative;
+display:none
+`;
+
+const Buttons = styled.button`
+display:flex;
+gap:8px;
+margin-top:40px;
+margin-left:14px;
+`;
+
+const No = styled.button`
+width: 85.17px;
+height: 37.17px;
+background: #FF3030;
+border-radius: 5px;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+display: flex;
+align-items: center;
+text-align: center;
+color: #FFFFFF;
+border:none
+`;
+
+const Almost = styled.button`
+width: 85.17px;
+height: 37.17px;
+background: #FF922E;
+border-radius: 5px;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+display: flex;
+align-items: center;
+text-align: center;
+color: #FFFFFF;
+border:none
+`;
+
+const Yes = styled.button`
+width: 85.17px;
+height: 37.17px;
+background: #2FBE34;
+border-radius: 5px;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 14px;
+text-align: center;
+color: #FFFFFF;
+border:none
+`;
